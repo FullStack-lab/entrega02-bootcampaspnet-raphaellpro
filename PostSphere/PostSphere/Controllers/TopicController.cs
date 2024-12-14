4b20c6c3bb0b5a03220f4b464658ab4152b727a3
raphaellpro <826.xxxxx@gmail.com>
2024-12-14 10:01:23 -0300

    feat: create topic list controller

---
+++ PostSphere/PostSphere/Controllers/TopicController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using PostSphere.Models;
+
+namespace PostSphere.Controllers
+{
+    public class TopicController : Controller
+    {
+        // Simulação de uma lista de tópicos
+        private static List<Topic> topics = new List<Topic>
+        {
+            new Topic { Id = 1, Title = "Problema com acesso ao sistema", Author = "Ana Souza", CreatedAt = DateTime.Now.AddHours(-5), ReplyCount = 3 },
+            new Topic { Id = 2, Title = "Sugestões de melhorias no layout", Author = "Carlos Lima", CreatedAt = DateTime.Now.AddDays(-1), ReplyCount = 7 },
+            new Topic { Id = 3, Title = "Erro ao exportar relatório", Author = "Mariana Silva", CreatedAt = DateTime.Now.AddMinutes(-30), ReplyCount = 1 },
+        };
+
+        // Action para exibir a lista de tópicos
+        public ActionResult TopicList()
+        {
+            return View(topics);
+        }
+    }
+}
