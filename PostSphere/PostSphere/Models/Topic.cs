b4c7c9e66e7bc51c27621e990ade030546c87179
raphaellpro <826.xxxxx@gmail.com>
2024-12-14 10:05:47 -0300

    feat: create model for topic list

---
+++ PostSphere/PostSphere/Models/Topic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PostSphere.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; } // Identificador único
+        public string Title { get; set; } // Título resumido do comentário principal
+        public string Author { get; set; } // Autor do comentário principal
+        public DateTime CreatedAt { get; set; } // Data e hora de criação
+        public int ReplyCount { get; set; } // Número de respostas
+    }
+}
