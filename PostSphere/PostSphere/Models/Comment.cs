8388ea4401fd7720347119fd126ca0e491597601
raphaellpro <826.xxxxx@gmail.com>
2024-12-14 14:49:47 -0300

    refactor: adjust model interaction (comment) so that hierarchy occurs

---
+++ PostSphere/PostSphere/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostSphere.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; } // ID do comentário
+        public string Text { get; set; } // Texto do comentário
+        public string Author { get; set; } // Nome do autor
+        public DateTime CreatedAt { get; set; } // Data de criação
+        public int? ParentId { get; set; } // Referência ao comentário pai
+        public Comment ParentComment { get; set; } // Comentário pai
+        public List<Comment> Replies { get; set; } // Respostas ao comentário
+
+        public Comment()
+        {
+            Replies = new List<Comment>();
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
